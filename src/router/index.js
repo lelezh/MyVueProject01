65a4d7295c9772f1f8713d36a6ad683a6c1b1dd9
lelezh <916464326@qq.com>
2019-03-13 16:17:29 +0800

    uadate

--- src/router/index.js
+++ src/router/index.js
@@ -4,30 +4,24 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 export default new Router({
-  mode:'history',
+ // mode:'history',
   routes: [
     {
       path:'/goods',
-      name:'goods',
-      base:'goods',
       component:(resolve)=>require(['../components/goods/goods'],resolve)
     },
     {
       path:'/ratings',
-      name:'ratings',
-      base:'ratings',
+
       component:(resolve)=>require(['../components/ratings/ratings'],resolve)
     },
     {
       path:'/seller',
-      name:'seller',
-      base:'seller',
+
       component:(resolve)=>require(['../components/seller/seller'],resolve)
     },
     {
       path:'*',
-      name:'goods',
-      base:'goods',
       redirect:'/goods'
     }
   ]

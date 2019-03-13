ce8ad338ba9c7d2e591494fdded0fe659c0113e5
lelezh <916464326@qq.com>
2019-03-13 15:18:05 +0800

    update

--- src/router/index.js
+++ src/router/index.js
@@ -9,21 +9,25 @@ export default new Router({
     {
       path:'/goods',
       name:'goods',
+      base:'goods',
       component:(resolve)=>require(['../components/goods/goods'],resolve)
     },
     {
       path:'/ratings',
       name:'ratings',
+      base:'ratings',
       component:(resolve)=>require(['../components/ratings/ratings'],resolve)
     },
     {
       path:'/seller',
       name:'seller',
+      base:'seller',
       component:(resolve)=>require(['../components/seller/seller'],resolve)
     },
     {
       path:'*',
       name:'goods',
+      base:'goods',
       redirect:'/goods'
     }
   ]
